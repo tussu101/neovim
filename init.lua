59aad28ac97cadcb7d14c6e7280198287d99f51e
tussu.101 <tussu.101@gmail.com>
2024-02-01 10:48:37 +0700

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+require("naphat.plugins-setup")
 require("naphat.core.options")
 require("naphat.core.keymaps")
 require("naphat.core.colorscheme")
+require("naphat.plugins.comment")
+require("naphat.plugins.nvim-tree")
+require("naphat.plugins.lualine")
