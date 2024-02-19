647a3f8530d33a2821dd4e81ac6371fab7d09669
tussu.101 <tussu.101@gmail.com>
2024-02-19 23:22:46 +0700

    update

--- lua/naphat/core/keymaps.lua
+++ lua/naphat/core/keymaps.lua
@@ -37,3 +37,5 @@ keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save current file" }) -- sa
 keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit current buffer" }) -- quit current buffer
 keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = "Force quit current buffer" }) -- force quit current buffer
 keymap.set("n", "<leader>x", "<cmd>wq<CR>", { desc = "Save and quit current buffer" }) -- save and quit current buffer
+
+keymap.set("n", "<leader>cn", ":%s/", { desc = "%s/oldName/newName/g" })
