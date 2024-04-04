f122e720f65460b2fed7f6039f38ce3dde5a8599
tussu.101 <tussu.101@gmail.com>
2024-04-04 22:11:47 +0700

    add plugins

---
+++ lua/naphat/plugins/todo-comments.lua
@@ -0,0 +1,28 @@
+return {
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
+
+		-- PERF: fully optimised
+		-- HACK: hmmm, this look a bit funky
+		-- TODO: What else?
+		-- NOTE: adding a note
+		-- FIX: This needs fixing
+		-- WARNING: ???
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
+
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
+
+		todo_comments.setup()
+	end,
+}
